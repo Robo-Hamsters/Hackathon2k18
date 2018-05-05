1c0380adf5880b69f1a08df164c9cc1e2a263842
Trojanzaro <anastasiszaro@gmail.com>
2018-05-05 10:15:15 +0300

    Changed Node and added FileGeolocImport

--- src/Map/Node.java
+++ src/Map/Node.java
@@ -5,34 +5,26 @@ import java.util.HashMap;
 public class Node {
 
    private double lat;
-
    private double lon;
+   private HashMap<Double,Node> distances = new HashMap<>();
 
    public double getLat() {
       return lat;
+   }
+   public double getLon() {
+      return lon;
+   }
+   public HashMap<Double, Node> getDestances() {
+      return distances;
    }
 
    public void setLat(double lat) {
       this.lat = lat;
    }
-
-   public double getLon() {
-      return lon;
-   }
-
    public void setLon(double lon) {
       this.lon = lon;
    }
-
-   public HashMap<Double, String> getDestances() {
-      return destances;
+   public void setDestances(HashMap<Double, Node> distances) {
+      this.distances = distances;
    }
-
-   public void setDestances(HashMap<Double, String> destances) {
-      this.destances = destances;
-   }
-
-   HashMap<Double,String> destances = new HashMap<>();
-
-
 }
