de161a07a56d70e0553ec938ccf6e2cd1848e95c
pavlmits <pav_mits@hotmail.com>
2018-05-05 10:11:04 +0300

    update class node

--- src/Map/Node.java
+++ src/Map/Node.java
@@ -8,7 +8,31 @@ public class Node {
 
    private double lon;
 
+   public double getLat() {
+      return lat;
+   }
+
+   public void setLat(double lat) {
+      this.lat = lat;
+   }
+
+   public double getLon() {
+      return lon;
+   }
+
+   public void setLon(double lon) {
+      this.lon = lon;
+   }
+
+   public HashMap<Double, String> getDestances() {
+      return destances;
+   }
+
+   public void setDestances(HashMap<Double, String> destances) {
+      this.destances = destances;
+   }
+
    HashMap<Double,String> destances = new HashMap<>();
-   
+
 
 }
