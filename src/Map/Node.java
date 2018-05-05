828b7b879563da420ba589bac1d54e790562482d
pavlmits <pav_mits@hotmail.com>
2018-05-05 10:09:47 +0300

    create class node

---
+++ src/Map/Node.java
@@ -0,0 +1,14 @@
+package Map;
+
+import java.util.HashMap;
+
+public class Node {
+
+   private double lat;
+
+   private double lon;
+
+   HashMap<Double,String> destances = new HashMap<>();
+   
+
+}
